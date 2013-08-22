12e9ab51d36650519295f416a7f3919d6656d8fd
Paolo Sberna <reliablebeaver86-cs@yahoo.it>
2013-08-22 12:37:21 +0200

    Write server

---
+++ server/geophoto.php
@@ -0,0 +1,55 @@
+<?php
+   // enable error reporting
+   ini_set('display_errors',1);
+   ini_set('display_startup_errors',1);
+   error_reporting(-1);
+   if(isset($_POST["data"])){
+     // time
+     date_default_timezone_set("Europe/Rome");
+     $time = date("Ymd-Gis");
+     // files
+     $xmlfn = $time.".xml";
+     $imagefn = $time.".jpg";
+     $xml = simplexml_load_string($_POST["data"]);
+     // save the image
+     $image = fopen($imagefn, "w");
+     fwrite($image, base64_decode($xml->image));
+     fclose($image);
+     // in the xml substitute the image with its filename
+     $xml->image = $imagefn;
+     // save the xml
+     $xml->asXML($xmlfn);
+   } else {
+   ?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>GeoPhoto</title>
+    <meta charset="UTF-8">
+  </head>
+  <body>
+    <ul>
+      <?php
+         $cwd = opendir(".");
+         while($filename = readdir($cwd)){
+           if(pathinfo($filename, PATHINFO_EXTENSION) == "xml"){
+             $xml = simplexml_load_file($filename);
+             echo '<li>';
+             echo '<div class="date">'.$xml->date."</div>\n";
+             echo '<div class="image"><a href="'.urlencode($xml->image)
+                  .'">image</a></div>';
+             echo '<div class="location"> Lat. '.$xml->location->latitude
+                 .', Long. '.$xml->location->longitude."</div>\n";
+             echo "</li>\n";
+           }
+         }
+         closedir($cwd);
+         ?>
+    </ul>
+    <form action="geophoto.php" method="post">
+      <input type="text" name="data"/>
+      <input type="submit"/>
+    </form>
+  </body>
+</html>
+<?php } ?>
